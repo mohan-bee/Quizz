e9c6407defd5d1689ed1a0b246103d17372caf7a
Mohan <167054270+mohan-bee@users.noreply.github.com>
2024-11-11 08:54:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,191 +1,20 @@
 const questions = {
     "question1": {
-        "q": "Why is it important to understand what happens 'below the surface' of a program?",
+        "q": "Nothing Here",
         "o0": {
-            "value": "To learn advanced application development techniques",
+            "value": "Ans 1",
             "isCorrect": false
         },
         "o1": {
-            "value": "To make high-level programming easier",
+            "value": "Ans 2",
             "isCorrect": false
         },
         "o2": {
-            "value": "To understand how the program is transformed into low-level instructions",
+            "value": "Ans 3",
             "isCorrect": true
         },
         "o3": {
-            "value": "To improve performance optimization",
-            "isCorrect": false
-        }
-    },
-    "question2": {
-        "q": "With the smartphones that we use on a daily basis to get our work done, we cannot see what is happening behind the applications. Which OOPS concept does this real-life scenario represent?",
-        "o0": {
-            "value": "Inheritance",
-            "isCorrect": false
-        },
-        "o1": {
-            "value": "Abstraction",
-            "isCorrect": true
-        },
-        "o2": {
-            "value": "Encapsulation",
-            "isCorrect": false
-        },
-        "o3": {
-            "value": "None of the options",
-            "isCorrect": false
-        }
-    },
-    "question3": {
-        "q": "Which data structure uses the principle of LIFO (Last-in First-out)?",
-        "o0": {
-            "value": "Stack",
-            "isCorrect": true
-        },
-        "o1": {
-            "value": "Queue",
-            "isCorrect": false
-        },
-        "o2": {
-            "value": "Array",
-            "isCorrect": false
-        },
-        "o3": {
-            "value": "None of the options",
-            "isCorrect": false
-        }
-    },
-    "question4": {
-        "q": "1, 2, 3, 4, and 5 are inserted in a queue one by one. After that, two elements were removed and 6, 7 were also inserted into the queue. What will the queue look like after these operations?",
-        "o0": {
-            "value": "2, 3, 4, 5, 6",
-            "isCorrect": false
-        },
-        "o1": {
-            "value": "3, 4, 5, 6, 7",
-            "isCorrect": true
-        },
-        "o2": {
-            "value": "1, 2, 3, 6, 7",
-            "isCorrect": false
-        },
-        "o3": {
-            "value": "None of the options",
-            "isCorrect": false
-        }
-    },
-    "question5": {
-        "q": "Ten elements are present in a queue initially. Three elements were removed, and then two more elements were removed, and then 1 element was added and then 2 more elements were removed. What will be the final length of the queue?",
-        "o0": {
-            "value": "6",
-            "isCorrect": false
-        },
-        "o1": {
-            "value": "5",
-            "isCorrect": true
-        },
-        "o2": {
-            "value": "4",
-            "isCorrect": false
-        },
-        "o3": {
-            "value": "10",
-            "isCorrect": false
-        }
-    },
-    "question6": {
-        "q": "What does the Pythagorean theorem help compute in the Point class example?",
-        "o0": {
-            "value": "The angle between two points",
-            "isCorrect": false
-        },
-        "o1": {
-            "value": "The area of a triangle formed by the points",
-            "isCorrect": false
-        },
-        "o2": {
-            "value": "The midpoint of two points",
-            "isCorrect": false
-        },
-        "o3": {
-            "value": "The distance between two points",
-            "isCorrect": true
-        }
-    },
-    "question7": {
-        "q": "I’m a pointer pointing to a memory location that has been deleted or freed. Who am I?",
-        "o0": {
-            "value": "Void Pointer",
-            "isCorrect": false
-        },
-        "o1": {
-            "value": "Wild Pointer",
-            "isCorrect": false
-        },
-        "o2": {
-            "value": "Dangling Pointer",
-            "isCorrect": true
-        },
-        "o3": {
-            "value": "None of the options",
-            "isCorrect": false
-        }
-    },
-    "question8": {
-        "q": "What is the main advantage of using a virtual machine (VM) in the context of Java?",
-        "o0": {
-            "value": "It speeds up the compilation process.",
-            "isCorrect": false
-        },
-        "o1": {
-            "value": "It eliminates the need for a compiler.",
-            "isCorrect": false
-        },
-        "o2": {
-            "value": "It allows the program to run on multiple platforms without modification.",
-            "isCorrect": true
-        },
-        "o3": {
-            "value": "It provides direct access to the hardware.",
-            "isCorrect": false
-        }
-    },
-    "question9": {
-        "q": "What is a significant benefit of breaking down the compilation process into two tiers?",
-        "o0": {
-            "value": "It reduces the overall execution time of programs.",
-            "isCorrect": false
-        },
-        "o1": {
-            "value": "It simplifies the development of compilers by dividing a complex task into simpler ones.",
-            "isCorrect": true
-        },
-        "o2": {
-            "value": "It allows for direct execution on hardware without additional translation.",
-            "isCorrect": false
-        },
-        "o3": {
-            "value": "It eliminates the need for multiple versions of the code.",
-            "isCorrect": false
-        }
-    },
-    "question10": {
-        "q": "How can high-level programmers ignore issues such as program execution, function call and return, handling loops (do, while), class functions, operating system intricacies, etc.?",
-        "o0": {
-            "value": "They treat the high-level language as an abstraction.",
-            "isCorrect": true
-        },
-        "o1": {
-            "value": "They focus solely on hardware intricacies to overlook programming challenges.",
-            "isCorrect": false
-        },
-        "o2": {
-            "value": "They rely on low-level language features to mitigate complex programming tasks.",
-            "isCorrect": false
-        },
-        "o3": {
-            "value": "They avoid using abstraction and prefer direct implementation for each software function.",
+            "value": "Ans 4",
             "isCorrect": false
         }
     }
